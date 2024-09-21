a797b7bb7e7151b0c28bcc8532dad478619c4c69
Ahmuktar <ahmuktar203@gmail.com>
2024-09-21 12:54:50 +0100

    changes

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ connectDB();
 // Use Routes
 app.use('/api', collectionRoutes);
 
+app.get('/', async (req, res) => {
+  res.json({ message: 'Welcome to KaPool Backend Api'});
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
